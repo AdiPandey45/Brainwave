d93e62c0c7baf79a4cc4bf6999dc15dc8d099372
AdiPandey45 <adipandey1405@gmail.com>
2025-04-25 00:55:54 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import { useState } from "react";
+import Header from './components/Header'
+import Button from "./components/Button";
+
+const App = () => {
+  return ( 
+    <>
+
+  
+     
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      
+      <Header/> 
+
+      </div>
+      <ButtonGradient />
+    
+    </>
+  );
+};
+
+export default App;
